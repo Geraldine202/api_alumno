e75b45e427e4c9f117edb587af1bde7ba6171f52
Geraldine202 <ge.inostrozam@duocuc.cl>
2026-07-22 15:47:57 -0400

    supabase

--- index.js
+++ index.js
@@ -371,6 +371,8 @@ app.post('/auth/login', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('API lista en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`API lista en el puerto ${PORT}`);
 });
